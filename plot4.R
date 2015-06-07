b5d50383fa216c1bf2a4034175dfbb8f2365c445
Kwame <atokubi@gmail.com>
2015-06-07 19:59:21 +0100

    Course project 1

--- plot4.R
+++ plot4.R
@@ -26,16 +26,16 @@ plot.chart <- function(df = read.data()){
         par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
         with(df, {
                 plot(Global_active_power~DateTime, type="l", 
-                     ylab="Global Active Power")
+                     ylab="Global Active Power", xlab = "")
                 plot(Voltage~DateTime, type="l", 
                      ylab="Voltage")
                 plot(Sub_metering_1~DateTime, type="l", 
-                     ylab="Energy sub metering")
+                     ylab="Energy sub metering", xlab = "")
                 lines(Sub_metering_2~DateTime,col='red')
                 lines(Sub_metering_3~DateTime,col='blue')
                 legend("topright", col=c("black", "red", "blue"), 
                        lty=1, lwd=2, bty="n", cex = 0.5,
                        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-                plot(Global_reactive_power~DateTime, type="l",)
+                plot(Global_reactive_power~DateTime, type="l")
         })
 }
